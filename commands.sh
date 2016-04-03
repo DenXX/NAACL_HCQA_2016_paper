cc01bf8eba4961ef9e97e5779473444c27683ff0
DenisSavenkov <denissavenkov@gmail.com>
2016-04-03 15:55:29 -0400

    Adding paper template files

---
+++ commands.sh
@@ -0,0 +1,6 @@
+git pull overleaf master
+git commit -a -m "Added ref to Smith et al. 2015"
+git push overleaf master
+
+git remote add github https://github.com/emory-irlab/paper_crowdsourcing_nonfactoidqa.git
+git push github
